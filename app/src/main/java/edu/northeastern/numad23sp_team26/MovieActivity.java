932db5361bbc33b0dd5e1c5fcbe3bd7062708080
Phyu Han <phyuhan77@gmail.com>
2023-02-17 17:35:43 -0500

    Adds API web service

--- app/src/main/java/edu/northeastern/numad23sp_team26/MovieActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/MovieActivity.java
@@ -3,15 +3,67 @@ package edu.northeastern.numad23sp_team26;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import edu.northeastern.numad23sp_team26.databinding.ActivityMovieBinding;
 
 public class MovieActivity extends AppCompatActivity {
+
+    private ActivityMovieBinding binding;
+    private static final String TAG = "MovieActivity";
 
     private final String omdbApiUrl = BuildConfig.OMDB_API_URL;
     private final String omdbApiKey = BuildConfig.OMDB_API_KEY;
 
+    private Handler resHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie);
+
+        binding = ActivityMovieBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Thread webThread = new WebServiceThread();
+        webThread.start();
+    }
+
+    private class WebServiceThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(omdbApiUrl + omdbApiKey + "&t=antman");
+                String res = NetworkUtil.httpResponse(url);
+
+                JSONObject jObject = new JSONObject(res);
+
+                resHandler.post(() -> {
+                    binding.webTextView.setText(jObject.toString());
+                });
+            } catch (MalformedURLException e) {
+                Log.e(TAG,"MalformedURLException");
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                Log.e(TAG,"ProtocolException");
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG,"IOException");
+                e.printStackTrace();
+            } catch (JSONException e) {
+                Log.e(TAG,"JSONException");
+                e.printStackTrace();
+            }
+        }
     }
 }
