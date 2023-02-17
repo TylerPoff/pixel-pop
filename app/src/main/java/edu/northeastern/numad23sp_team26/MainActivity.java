c054086ab3adb51364b7ee4e81f1f9c43eb225f8
TylerPoff <Tyler.Poff1900@gmail.com>
2023-02-17 12:55:13 -0500

    -Create Movie Button -"Hello World" to "Team 26"

--- app/src/main/java/edu/northeastern/numad23sp_team26/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/MainActivity.java
@@ -2,7 +2,9 @@ package edu.northeastern.numad23sp_team26;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnMovie = findViewById(R.id.btnMovie);
+        btnMovie.setOnClickListener(v -> openActivityMovie());
     }
-
+    public void openActivityMovie() {
+        Intent intentAboutMe = new Intent(this, MovieActivity.class);
+        startActivity(intentAboutMe);
+    }
 }
---
+++ app/src/main/java/edu/northeastern/numad23sp_team26/MovieActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_team26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
