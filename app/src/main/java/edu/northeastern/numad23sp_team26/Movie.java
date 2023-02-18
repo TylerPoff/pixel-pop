6e45a67f1e4dddd5e18abcf34c87d6c4ef1c4483
eliokhouri <elliotkhouri@gmail.com>
2023-02-18 15:05:07 -0800

    PR Review Changes
    Restored the activity_movie layout file + changed the type of year to int in the movie java file.

--- app/src/main/java/edu/northeastern/numad23sp_team26/Movie.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/Movie.java
@@ -4,13 +4,13 @@ public class Movie {
     private final String genre;
     private final String posterURL;
     private final String name;
-    private final String year;
+    private final int year;
     private final String imdbID;
     private final String type;
 
 
-    public Movie(String name, String genre, String year, String imdbID
-                 ,String type, String posterURL) {
+    public Movie(String name, String genre, int year, String imdbID,
+                 String type, String posterURL) {
         this.genre = genre;
         this.posterURL = posterURL;
         this.name = name;
@@ -26,7 +26,7 @@ public class Movie {
     public String getName() {
         return this.name;
     }
-    public String getYear() {
+    public int getYear() {
         return this.year;
     }
     public String getType() { return this.type; }
--- app/src/main/java/edu/northeastern/numad23sp_team26/MovieActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/MovieActivity.java
@@ -9,6 +9,6 @@ public class MovieActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.movie_card_layout);
+        setContentView(R.layout.activity_movie);
     }
 }
