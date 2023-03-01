ce73c47b30b62545bbd1a44f5edebfcaa7b47211
Phyu Han <phyuhan77@gmail.com>
2023-03-01 14:45:08 -0500

    Adds strict mode and firebase database

---
+++ app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/HomeActivity.java
@@ -0,0 +1,15 @@
+package edu.northeastern.numad23sp_team26.a8_stickers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "a8_stickers.HomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/models/User.java
@@ -0,0 +1,4 @@
+package edu.northeastern.numad23sp_team26.a8_stickers.models;
+
+public class User {
+}
