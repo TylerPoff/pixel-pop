6e89bd1c64d1d65e8dbb80db85c0d3c70e5a6ad7
eliokhouri <elliotkhouri@gmail.com>
2023-03-02 01:00:42 -0800

    added about button to main activity, about button now launches a new activity with team member info.

--- app/src/main/java/edu/northeastern/numad23sp_team26/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnMovie = findViewById(R.id.btnMovie);
-        btnMovie.setOnClickListener(v -> openActivityMovie());
+
+        /*Button btnMovie = findViewById(R.id.btnMovie);
+        btnMovie.setOnClickListener(v -> openActivityMovie());*/
     }
 
-    public void openActivityMovie() {
+    /*public void openActivityMovie() {
         Intent intent = new Intent(this, MovieActivity.class);
         startActivity(intent);
+    }*/
+
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.btnMovie) {
+            Intent MovieActivityintent = new Intent(getApplicationContext(), MovieActivity.class);
+            startActivity(MovieActivityintent);
+        }
+        else if (v.getId() == R.id.btnAbout) {
+            Intent AboutActivityintent = new Intent(getApplicationContext(), AboutActivity.class);
+            startActivity(AboutActivityintent);
+        }
     }
 }
