6221a751388483f0f8d8892edbf260b5260e0fb9
eliokhouri <elliotkhouri@gmail.com>
2023-03-02 01:43:04 -0800

    created new activity and layout files
    Stickers list and received history.

---
+++ app/src/main/java/edu/northeastern/numad23sp_team26/StickersListActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_team26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StickersListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stickers_list);
+    }
+}
