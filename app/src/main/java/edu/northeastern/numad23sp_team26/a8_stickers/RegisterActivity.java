37ed84940ff18f2da487c6065459a1bc36c9cbb7
tharamesseroux <messeroux.t@northeastern.edu>
2023-03-03 18:00:37 -0500

    Checking if Username, FirstName or LastName is empty or not

--- app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/RegisterActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/RegisterActivity.java
@@ -3,7 +3,12 @@ package edu.northeastern.numad23sp_team26.a8_stickers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,14 +16,44 @@ import edu.northeastern.numad23sp_team26.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private static final String TAG = "a8_stickers.RegisterActivity";
+    TextInputEditText editTextUsername, editTextFirstName, editTextLastName;
+    Button buttonReg;
 
+
+    private static final String TAG = "a8_stickers.RegisterActivity";
     private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextUsername = findViewById(R.id.username);
+        buttonReg = findViewById(R.id.btnRegister);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username, firstName, lastName;
+                username = String.valueOf(editTextUsername.getText());
+                firstName = String.valueOf(editTextFirstName.getText());
+                lastName = String.valueOf(editTextLastName.getText());
+                /** Checking if Username, FirstName or LastName is empty or not*/
+                if (TextUtils.isEmpty(username)){
+                    Toast.makeText(RegisterActivity.this, "Please, enter username", Toast.LENGTH_SHORT).show();
+                }
+
+                if (TextUtils.isEmpty(firstName)){
+                    Toast.makeText(RegisterActivity.this, "Please, enter first name", Toast.LENGTH_SHORT).show();
+                }
+
+                if (TextUtils.isEmpty(firstName)){
+                    Toast.makeText(RegisterActivity.this, "Please, enter last name", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
     }
