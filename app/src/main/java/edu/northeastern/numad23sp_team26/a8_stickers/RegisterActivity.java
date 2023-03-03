e44fe248dcc63d1543f93a3c0eba99134e606fad
tharamesseroux <messeroux.t@northeastern.edu>
2023-03-03 18:19:25 -0500

    create a new account by passing the new user's email address and password to createUserWithEmailAndPassword
    todo: make with username, first name and lastname instead

--- app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/RegisterActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/RegisterActivity.java
@@ -1,14 +1,21 @@
 package edu.northeastern.numad23sp_team26.a8_stickers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,6 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     TextInputEditText editTextUsername, editTextFirstName, editTextLastName;
     Button buttonReg;
+    FirebaseAuth mAuth;
 
 
     private static final String TAG = "a8_stickers.RegisterActivity";
@@ -27,6 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
         editTextUsername = findViewById(R.id.username);
         buttonReg = findViewById(R.id.btnRegister);
 
@@ -40,16 +49,37 @@ public class RegisterActivity extends AppCompatActivity {
 
                 if (TextUtils.isEmpty(username)){
                     Toast.makeText(RegisterActivity.this, "Please, enter username", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(firstName)){
                     Toast.makeText(RegisterActivity.this, "Please, enter first name", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 if (TextUtils.isEmpty(firstName)){
                     Toast.makeText(RegisterActivity.this, "Please, enter last name", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
+                /** implemented the code for creating the user
+                 * create a new account by passing the new user's email address and password to createUserWithEmailAndPassword: */
+                mAuth.createUserWithEmailAndPassword(username, firstName, lastName)
+                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    // Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    // Log.w(TAG, "createUserWithUserName:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 
