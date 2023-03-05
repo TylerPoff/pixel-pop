ec575ba26027be37b1698e6f855d4d3dc1449d5a
eliokhouri <elliotkhouri@gmail.com>
2023-03-04 18:29:08 -0800

    adapter and viewholder
    added content

--- app/src/main/java/edu/northeastern/numad23sp_team26/ReceivedHistoryAdapter.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/ReceivedHistoryAdapter.java
@@ -1,4 +1,38 @@
 package edu.northeastern.numad23sp_team26;
 
-public class ReceivedHistoryAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ReceivedHistoryAdapter extends RecyclerView.Adapter<ReceivedHistoryViewHolder> {
+
+    private final List<ReceivedHistory> receivedHistories;
+    private final Context context;
+
+    public ReceivedHistoryAdapter(List<ReceivedHistory> receivedHistories, Context context) {
+        this.receivedHistories = receivedHistories;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ReceivedHistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ReceivedHistoryViewHolder(LayoutInflater.from(context)
+                .inflate(R.layout.sticker_card_layout, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReceivedHistoryViewHolder holder, int position) {
+        holder.bindThisData(receivedHistories.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return receivedHistories.size();
+    }
 }
--- app/src/main/java/edu/northeastern/numad23sp_team26/ReceivedHistoryViewHolder.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/ReceivedHistoryViewHolder.java
@@ -1,4 +1,30 @@
 package edu.northeastern.numad23sp_team26;
 
-public class ReceivedHistoryViewHolder {
+import android.content.Context;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ReceivedHistoryViewHolder  extends RecyclerView.ViewHolder {
+
+    Context context;
+    private static final String TAG = "RecievedHistoryViewHolder";
+    public TextView stickerNameTV, stickerFromTV, stickerTimestampTV;
+    public ImageView stickerIV;
+    private Handler handler = new Handler();
+
+    public ReceivedHistoryViewHolder(@NonNull View itemView) {
+        super(itemView);
+        context = itemView.getContext();
+        stickerNameTV = itemView.findViewById(R.id.stickerNameTV);
+        stickerFromTV = itemView.findViewById(R.id.fromTV);
+        stickerTimestampTV = itemView.findViewById(R.id.timestampTV);
+        stickerIV = itemView.findViewById(R.id.stickerIV);
+
+    }
+
 }
