3b34a3f803779d2a6cb3cd52a22ec58120f016a6
TylerPoff <Tyler.Poff1900@gmail.com>
2023-03-05 10:48:04 -0500

    Create basics of ReceiveNotificationActivity

---
+++ app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/ReceiveNotificationActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad23sp_team26.a8_stickers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.numad23sp_team26.R;
+
+public class ReceiveNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_notification);
+    }
+}
