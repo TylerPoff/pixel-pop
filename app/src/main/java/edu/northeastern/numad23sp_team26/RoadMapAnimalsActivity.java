7258b6bb513050a5cc6077b999b30154ff0698a9
tharamesseroux <messeroux.t@northeastern.edu>
2023-04-03 15:43:48 -0400

    landscape phone design added for all 6 selections

--- app/src/main/java/edu/northeastern/numad23sp_team26/RoadMapAnimalsActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/RoadMapAnimalsActivity.java
@@ -2,8 +2,10 @@ package edu.northeastern.numad23sp_team26;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class RoadMapAnimalsActivity extends AppCompatActivity {
 
@@ -12,10 +14,15 @@ public class RoadMapAnimalsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_road_map_animals);
 
-//        View view = findviewbyid(R.id.button1);
-//        view.setOnClickListener( v -> {
-//            Intent intet = intent for new activity;
-//            startactivity(intent);
+//        Button animals_button1 = findViewById(R.id.animals_button1);
+//
+//        animals_button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(RoadMapAnimalsActivity.this, AnimalsPixelDrawing1Activity.class);
+//                startActivity(intent);
+//            }
 //        });
+
     }
 }
