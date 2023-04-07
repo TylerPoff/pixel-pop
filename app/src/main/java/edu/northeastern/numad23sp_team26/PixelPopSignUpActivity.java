80353d21d90b0ba59804e218c8c53b369148d520
Phyu Han <phyuhan77@gmail.com>
2023-04-07 19:13:29 -0400

    Clean up

--- app/src/main/java/edu/northeastern/numad23sp_team26/PixelPopSignUpActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/PixelPopSignUpActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,8 +14,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import edu.northeastern.numad23sp_team26.a8_stickers.LoginActivity;
 
 public class PixelPopSignUpActivity extends AppCompatActivity {
 
