9d4358f64ffedf6a7e7ec0cf4bb71752aeff54ad
tharamesseroux <messeroux.t@northeastern.edu>
2023-04-06 20:01:28 -0400

    added the pixel images for the animals and avatar profile category. I'll clean up the files again soon and make sure not to delete anything important
    Todo
    -> creating pixel images
    -> linking those pixel images
    -> make buttons white
    -> populate the buttons in the portrait and landscape mode
    -> make buttons change color once the level is done

--- app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/RegisterActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_team26/a8_stickers/RegisterActivity.java
@@ -64,6 +64,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    // test
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
